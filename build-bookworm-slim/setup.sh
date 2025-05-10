dd2f366cab5e0c3db471ed548b5f3e956b859c66
Sam M <git@s-mc.io>
2025-05-11 09:58:48 +1200

    make user group

--- build-bookworm-slim/setup.sh
+++ build-bookworm-slim/setup.sh
@@ -6,3 +6,5 @@ apt-get install -y openssl unzip curl
 curl -fsSL https://bun.sh/install | bash
 
 rm -rf /var/lib/apt/lists/*
+
+useradd -G kea-build --uid 1000 --gid 1000 --shell /bin/bash --create-home
