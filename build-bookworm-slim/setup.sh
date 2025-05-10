c45fc0aa986f187e13ea4a2a15124a63a3d3d839
Sam M <git@s-mc.io>
2025-05-11 10:04:59 +1200

    fix setup

--- build-bookworm-slim/setup.sh
+++ build-bookworm-slim/setup.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-apt update
-
-apt-get install -y openssl unzip curl
 curl -fsSL https://bun.sh/install | bash
-
-rm -rf /var/lib/apt/lists/*
-
-useradd -G kea-build --uid 1000 --gid 1000 --shell /bin/bash --create-home
--- run-bookworm-slim/setup.sh
+++ run-bookworm-slim/setup.sh
@@ -1,10 +1,1 @@
 #!/usr/bin/env bash
-
-apt update
-
-apt-get install -y openssl unzip curl
-curl -fsSL https://bun.sh/install | bash
-
-rm -rf /var/lib/apt/lists/*
-
-useradd -G kea --uid 1001 --gid 1001 --shell /bin/bash --create-home
